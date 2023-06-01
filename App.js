ac81541af696961961bb3254a16aca366d4f9638
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-06-01 10:40:13 +0500

    .

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-gesture-handler'
 import { FontAwesome } from "@expo/vector-icons"
-
 
 import Login from './screens/login_screen';
 import SignUp from './screens/signup_screen';
@@ -41,7 +37,7 @@ function Drawer({ navigation }) {
         title: 'Home',
         // --- Adjusting Header Style
         headerStyle: {
-          backgroundColor: 'lightblue',
+          backgroundColor: 'red',
         },
         
       }} name='Home' component={Home} />
